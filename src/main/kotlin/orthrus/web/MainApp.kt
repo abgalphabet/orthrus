70e51e67ecae4c0a8040f3506d9e1a58137f74c5
arthur <arthur.mw.yeung@gmail.com>
2017-10-01 23:14:54 +0800

    add https

--- src/main/kotlin/orthrus/web/MainApp.kt
+++ src/main/kotlin/orthrus/web/MainApp.kt
@@ -1,32 +1,53 @@
 package orthrus.web
 
 import orthrus.conf.MainModule
+import orthrus.web.handlers.Handlers
 import ratpack.guice.Guice
 import ratpack.server.RatpackServer
-import ratpack.server.RatpackServerSpec
-import orthrus.web.handlers.Handlers
+import ratpack.ssl.SSLContexts
 
 /**
  * Created by arthur on 14/7/2017.
  */
 class MainApp: App {
-    val server: RatpackServer
+    val http: RatpackServer
+    val https: RatpackServer
 
     init {
         val injector = com.google.inject.Guice.createInjector(MainModule())
-        server = RatpackServer.of({ serverSpec: RatpackServerSpec ->
+        http = RatpackServer.of { serverSpec ->
             serverSpec
                 .registry(Guice.registry(injector))
                 .handlers(Handlers())
-        })
+                .serverConfig {
+                    it.port(5080)
+                }
+        }
+
+        https = RatpackServer.of { serverSpec ->
+            serverSpec
+                .registry(Guice.registry(injector))
+                .handlers(Handlers())
+                .serverConfig { serverConfigBuilder ->
+                    val keystore = MainApp.javaClass.getResource("/pki/keystore.p12")
+                    val truststore = MainApp.javaClass.getResource("/pki/truststore.p12")
+
+                    val sslContext = SSLContexts.sslContext(keystore, "passw0rd", truststore, "passw0rd")
+                    serverConfigBuilder
+                        .ssl(sslContext)
+                        .port(5443)
+                }
+        }
     }
 
     override fun start() {
-        server.start()
+        http.start()
+        https.start()
     }
 
     override fun stop() {
-        server.stop()
+        http.stop()
+        https.stop()
     }
 
     companion object {
